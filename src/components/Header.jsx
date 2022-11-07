23b6a9ab7da6252a6c7e618db25ce4a77ef388ab
damin <damin0320@gmail.com>
2022-11-07 23:43:50 +0900

    ADD : madal 기초공사

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Modal from './modal'
+
+const Header = () => {
+  return (
+    <div>
+      <Modal/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/modal.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import styled from "styled-components"
+
+const Modal = () => {
+  return (
+    <Background>
+      <Content>
+      </Content>
+    </Background>
+  )
+}
+
+export default Modal
+
+//아래는 styled-components를 통한 스타일링
+
+const Background = styled.div`
+  height: 510px;
+  width: 606px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+`
+
+const Content = styled.div`
+  height: 100%;
+  width: 950px;
+  margin-top: 70px;
+  position: relative;
+  top: 25%;
+  left: 100%;
+  overflow: none;
+  background: #141414;
+  box-shadow: 0px 4px 8px #eee;
+`
