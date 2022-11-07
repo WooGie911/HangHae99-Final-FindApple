af630b51a156e4e81024f5d56358f3826592e5ab
damin <damin0320@gmail.com>
2022-11-07 18:24:14 +0900

    ADD : 초기세팅

---
+++ src/redux/config/configStore.js
@@ -0,0 +1,21 @@
+// src/redux/modules/config/configStore.js
+
+import { configureStore } from "@reduxjs/toolkit";
+
+
+import posts from "../modules/PostsSlice";
+import details from "../modules/PostDetailsSlice";
+import objections from "../modules/ObjectionsSlice";
+import objectionDetails from "../modules/ObjectionDetailsSlice";
+import account from "../modules/LoginSlice";
+
+
+const store = configureStore({
+
+  reducer: { posts , account , details, objections, objectionDetails },
+
+  //dev tool을 개발 환경에서만 설정
+  // devTools: process.env.REACT_APP_MOD !== "production",
+});
+
+export default store;
---
+++ src/shared/Router.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import PostCreate from "../pages/PostCreate";
+import PostRead from "../pages/PostRead"
+import PostUpdate from "../pages/PostUpdate"
+import PostDetail from "../pages/PostDetail"
+import ObjectionCreate from "../pages/ObjectionCreate"
+import ObjectionRead from "../pages/ObjectionRead"
+import ObjectionUpdate from "../pages/ObjectionUpdate"
+import ObjectionDetail from "../pages/ObjectionDetail"
+import Mypage from "../pages/Mypage"
+import MypageUpdate from "../pages/MypageUpdate"
+import Main from "../pages/Main"
+
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 로그인 페이지 이동하기 */}
+        <Route path='/sigin' element={<SignIn/>} />
+
+        {/* 회원가입 페이지 이동하기 */}
+        <Route path='/signup' element={<SignUp />} />
+
+        {/* 포스트 리스트 생성 페이지로 이동하기 */}
+        <Route path='/postcreate' element={<PostCreate />} />
+        
+        {/* 게시글 홈으로 이동하기 */}
+        <Route path='/postread' element={<PostRead />} />
+
+        {/* 게시글 업데이트로 이동하기 */}
+        <Route path='/postupdate' element={<PostUpdate />} />
+
+        {/* 포스트리스트 카드별 상세보기 페이지로 이동하기 */}
+        <Route path='/PostDetail/:id' element={<PostDetail />} />
+
+        {/* 이의제기 리스트 생성 페이지로 이동하기 */}
+        <Route path='/objectioncreate' element={<ObjectionCreate />} />
+        
+        {/* 이의제기 홈으로 이동하기 */}
+        <Route path='/objectionread' element={<ObjectionRead />} />
+
+        {/* 이의제기 업데이트로 이동하기 */}
+        <Route path='/objectionupdate' element={<ObjectionUpdate />} />
+
+        {/* 이의제기리스트 카드별 상세보기 페이지로 이동하기 */}
+        <Route path='objectionDetail/:id' element={<ObjectionDetail />} />        
+
+        {/* 개인 페이지 이동하기 */}
+        <Route path='/mypage' element={<Mypage />} />
+
+        {/* 개인 페이지 업데이트 이동하기 */}
+        <Route path='/mypageupdate' element={<MypageUpdate />} />
+
+        {/* main 페이지 이동하기 */}
+        <Route path='/' element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
