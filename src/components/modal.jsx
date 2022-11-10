1ca7b7c86cd4f708c4fe86dc03f31430327075bc
rlawodnr1109 <wodnr1109@naver.com>
2022-11-11 01:28:51 +0900

    이의제기 이동

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,34 +1,47 @@
-import React, {useState} from "react"
-import styled from "styled-components"
-import PricingInput from "./PricingInput"
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import PricingInput from "./PricingInput";
+import { useNavigate } from "react-router-dom";
 
 const Modal = (props) => {
- // 토글
- const [toggle, setToggle] = useState(true)
+  // 토글
+  const [toggle, setToggle] = useState(true);
+  const navigate = useNavigate();
 
- // 토글 핸들러
- const toggleBtnHandle = () => {
-   setToggle(!toggle)
- }
+  // 토글 핸들러
+  const toggleBtnHandle = () => {
+    setToggle(!toggle);
+  };
 
- const handleModal = () => {
-   props.setModalOn(false)
- }
+  const handleModal = () => {
+    props.setModalOn(false);
+  };
 
   return (
     <Background>
       <Content>
         <button onClick={handleModal}>모달끄기</button>
-        <button>상품등록</button>
-        <button>이의제기</button>
-        <PricingInput/>
+        <button
+          onClick={() => {
+            navigate("/postcreate");
+          }}
+        >
+          상품등록
+        </button>
+        <button
+          onClick={() => {
+            navigate("/objectionread");
+          }}
+        >
+          이의제기
+        </button>
+        <PricingInput />
       </Content>
     </Background>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
 
 //아래는 styled-components를 통한 스타일링
 
@@ -41,7 +54,7 @@ const Background = styled.div`
   position: fixed;
   left: 0;
   top: 0;
-`
+`;
 
 const Content = styled.div`
   height: 100%;
@@ -54,4 +67,4 @@ const Content = styled.div`
   background: gray;
   box-shadow: 0px 4px 8px #eee;
   z-index: 999;
-`
+`;
