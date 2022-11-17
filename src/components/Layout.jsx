5aa01615060cbcbf0f7a3d61a42f78875788fbad
damin <damin0320@gmail.com>
2022-11-17 23:46:11 +0900

    FIX: Layout, Footer 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,18 +3,22 @@ import styled from "styled-components";
 
 
 const Layout = ({ children }) => {
-  return <StLayout>{children}</StLayout>;
-};
+  return (
+    <StLayout>
+    {children}
+    </StLayout>
+    )
+  };
 
 
 export default Layout
 
 const StLayout = styled.div`
+  
   max-width: 375px;
   max-height: 100vh;
+  min-height: 100vh;
   height : 100%;
   overflow: auto;
-  padding : 10px;
   background-color: aqua;
-  /* padding : 0px 20px; */
 `;
