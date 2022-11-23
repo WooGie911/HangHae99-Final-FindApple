ac98828e9727f1954883bd38176982590729a550
damin <damin0320@gmail.com>
2022-11-23 18:55:06 +0900

    ADD : 마이페이지 내가 쓴 글 수정

--- src/pages/MyPost.jsx
+++ src/pages/MyPost.jsx
@@ -1,33 +1,55 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { __getMyPost } from "../redux/modules/MypageSlice";
+import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import Footer from "../components/Footer";
+import styled from "styled-components";
+import back from "../assets/back.png";
+
 
 const MyPost = () => {
   const { posts } = useSelector((state) => state.mypage);
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(__getMyPost());
   }, []);
+  const onClickHandler = () => {
+    navigate(-1);
+  };
   return (
     <div>
       <Layout>
-        MyPost
+      <HeadContainer>
+          <div>
+            <img
+              onClick={onClickHandler}
+              style={{ width: 25, height: 25 }}
+              src={back}
+            />
+            <span>내가 쓴 글</span>
+          </div>
+        </HeadContainer>
+        <hr />
+        <PostList>
+              <Posts>
         {posts.length > 0 && (
           <div>
             {posts.map((post) => {
               return (
-                <div key={post.postId}>
-                  <img src={post.images[0].imgUrl} />
+                <SellerPost key={post.postId}>
+                  <img src={post.images[0].imgUrl} onClick={() => {navigate(`/PostDetail/${post.postId}`)}} />
                   <div>{post.title}</div>
-                  <div>{post.userPrice}</div>
-                </div>
+                  <div>{post.userPrice}원</div>
+                </SellerPost>
               );
             })}
           </div>
         )}
+                      </Posts>
+            </PostList>
         <Footer />
       </Layout>
     </div>
@@ -35,3 +57,48 @@ const MyPost = () => {
 };
 
 export default MyPost;
+//헤더
+const HeadContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 120px;
+  margin-bottom: 20px;
+  img {
+    float: left;
+    margin-right: 110px;
+  }
+  span {
+    /* text-align: center; */
+    font-size: 24px;
+    font-weight: bold;
+  }
+`;
+
+// 아이템
+
+const PostList = styled.div`
+  position: relative;
+  width: 100%;
+  height: 80vh;
+  overflow: auto;
+  background-color: white;
+  border-radius: 5px 5px 0 0;
+  padding-bottom: 140px;
+`;
+
+const Posts = styled.div`
+  margin-bottom: 50px;
+`;
+
+const SellerPost = styled.div`
+  float: left;
+  margin-left: 20px;
+  img {
+    margin: auto;
+    position: relative;
+    display: flex;
+    width: 160px;
+    height: 160px;
+    border-radius: 5px;
+  }
+`;
