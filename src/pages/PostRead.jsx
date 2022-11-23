f3f9be08fb12a340796b80493566b8d7aa9a2624
damin <damin0320@gmail.com>
2022-11-24 02:30:24 +0900

    ADD: 최신순 인기순 전체개수 조회 완료

--- src/pages/PostRead.jsx
+++ src/pages/PostRead.jsx
@@ -15,6 +15,7 @@ const PostRead = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.posts);
+  const {postsCount} = useSelector((state) => state.posts)
   const { HeaderState } = useSelector((state) => state.posts);
   const [submitObj, setSubmitObj] = useState(HeaderState);
 
@@ -34,6 +35,7 @@ const PostRead = () => {
           Navigate={"/postread"}
         />
         <SortBar
+          postsCount={postsCount}
           state={submitObj}
           setState={setSubmitObj}
           Navigate={"/postread"}
