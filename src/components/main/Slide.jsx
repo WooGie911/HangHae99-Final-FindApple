f3a3c206326ea762f49d8acf4e0b501a7c2e27ff
damin <damin0320@gmail.com>
2022-12-08 20:38:28 +0900

    FIX : slick slider 단추 색깔 변경

--- src/components/main/Slide.jsx
+++ src/components/main/Slide.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import banner1 from "../../assets/banner1.png";
 import banner2 from "../../assets/banner2.png";
 import banner3 from "../../assets/banner3.png";
+import "../../assets/css/slide.css"
 
 const Slide = () => {
   const settings = {
