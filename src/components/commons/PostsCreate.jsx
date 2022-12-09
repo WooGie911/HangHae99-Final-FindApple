12a0d324cb3eeec07b9271007fde35c0fc285f79
damin <damin0320@gmail.com>
2022-12-09 18:20:41 +0900

    FIX: 돈단위 다시 변경 및 alert 수정

--- src/components/commons/PostsCreate.jsx
+++ src/components/commons/PostsCreate.jsx
@@ -16,6 +16,7 @@ const PostsCreate = (props) => {
     userPrice: "",
     content: "",
   });
+  console.log(writeHandle)
 
   //이미지 업로드 훅
   const [files, fileUrls, uploadHandle] = useImgUpload(3, true, 4, 1000);
@@ -70,6 +71,12 @@ const PostsCreate = (props) => {
     if (write.title === "") {
       return window.alert("제목을 입력하세요");
     }
+
+      // 돈 단위 alert
+      if (write.userPrice > 9999999) {
+        return window.alert("가격이 너무 높습니다.")
+      }
+
     if (props.post === "post") {
       if (write.userPrice === "") {
         return window.alert("판매가격을 입력하세요");
@@ -78,7 +85,7 @@ const PostsCreate = (props) => {
       if (write.userPrice === "") {
         return window.alert("희망가격을 입력하세요");
       }
-    }
+    } 
 
     if (write.content === "") {
       return window.alert("상품설명을 입력하세요");
@@ -89,6 +96,7 @@ const PostsCreate = (props) => {
       // navigate(`/${props.Navigate}`);
       // window.location.reload(`/${props.Navigate}`);
     }
+
   };
   const onClickHandler = () => {
     navigate(-1);
