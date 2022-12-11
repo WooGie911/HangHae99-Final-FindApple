a8fc503a10c8ec8fc7d6b88c9d8b4c9ca1879ece
rlawodnr1109 <wodnr1109@naver.com>
2022-12-12 01:29:50 +0900

    채팅 리스트 CSS 수정

--- src/pages/chatting/element/ChatList.jsx
+++ src/pages/chatting/element/ChatList.jsx
@@ -26,61 +26,78 @@ const ChatList = () => {
 
   return (
     <div className=" w-[375px] px-4">
+      <div className="border-b-[0.5px] border-D9 h-10 bg-white flex rounded-t-md  items-center px-3 text-sm font-semibold">
+        채팅
+      </div>
       <div
-        className="bg-white rounded-lg "
+        className="bg-white rounded-b-md h-80 overflow-auto scrollbar-hide"
         onClick={(e) => e.stopPropagation()}
       >
         {Room !== undefined &&
-          Room !== [] &&
-          Room.map((item, i) => {
-            return (
-              <>
-                {item.chatList.length > 0 && (
-                  <div className=" px-3 py-1 " key={i}>
-                    <div
-                      className="  flex justify-between items-center h-14 "
-                      onClick={() => onClickChatting(item)}
-                    >
-                      <div className="flex  items-center w-full">
-                        <img
-                          className="w-11 h-11 rounded-full object-cover flex-shrink-0"
-                          src={item.otherUserAvatarUrl}
-                        />
-                        <div className=" ml -2 flex-col text-sm ">
-                          <div className="flex">
-                            <div className="font-semibold">
-                              {item.otherNickname.length > 10
-                                ? item.otherNickname.substring(0, 9) + "..."
-                                : item.otherNickname}
-                            </div>
-                            <div className="text-OO text-xs">
-                              {item.chatList[item.chatList.length - 1].sendDate}
+          (Room.length > 0 ? (
+            Room.map((item, i) => {
+              return (
+                <>
+                  {item.chatList.length > 0 && (
+                    <>
+                      <div className=" px-3 py-1 " key={i}>
+                        <div
+                          className=" flex justify-between items-center h-14 "
+                          onClick={() => onClickChatting(item)}
+                        >
+                          <div className="flex  items-center w-full">
+                            <img
+                              className="w-11 h-11 rounded-full object-cover flex-shrink-0"
+                              src={item.otherUserAvatarUrl}
+                            />
+                            <div className=" ml-2 flex-col text-sm ">
+                              <div className="flex items-center">
+                                <div className="font-semibold">
+                                  {item.otherNickname.length > 10
+                                    ? item.otherNickname.substring(0, 9) + "..."
+                                    : item.otherNickname}
+                                </div>
+                                <div className="ml-1 text-OO text-xs">
+                                  {
+                                    item.chatList[item.chatList.length - 1]
+                                      .sendDate
+                                  }
+                                </div>
+                              </div>
+                              <div className="text-xs">
+                                {item.chatList[item.chatList.length - 1].message
+                                  .length > 20
+                                  ? item.chatList[
+                                      item.chatList.length - 1
+                                    ].message.substring(0, 19) + "..."
+                                  : item.chatList[item.chatList.length - 1]
+                                      .message}
+                              </div>
                             </div>
                           </div>
-                          <div className="text-xs">
-                            {item.chatList[item.chatList.length - 1].message
-                              .length > 20
-                              ? item.chatList[
-                                  item.chatList.length - 1
-                                ].message.substring(0, 19) + "..."
-                              : item.chatList[item.chatList.length - 1].message}
-                          </div>
+
+                          {item.image !== undefined && (
+                            <img
+                              className="w-11 h-11 object-cover rounded-lg flex-shrink-0"
+                              src={item.image.imgUrl}
+                            />
+                          )}
                         </div>
                       </div>
-
-                      {item.image !== undefined && (
-                        <img
-                          className="w-11 h-11 object-cover rounded-lg flex-shrink-0"
-                          src={item.image.imgUrl}
-                        />
-                      )}
-                    </div>
-                  </div>
-                )}
-                <hr className="last-of-type:hidden border-t-[0.5px] border-D9" />
-              </>
-            );
-          })}
+                      <hr className="last-of-type:hidden border-t-[0.5px] border-D9" />
+                    </>
+                  )}
+                </>
+              );
+            })
+          ) : (
+            <div className="flex justify-center items-center h-full text-D9">
+              <div>
+                <div className="flex justify-center">아직 채팅이 없어요 </div>
+                <div>유저들과 채팅을 시작해보세요!</div>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
